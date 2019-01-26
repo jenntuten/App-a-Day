e1d734407cb158d72699a9e826bd663a84d9a0b9
jenn210 <jennifer.c.tuten@gmail.com>
2019-01-26 13:23:04 -0600

    ??

--- server.js
+++ server.js
@@ -34,9 +34,6 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/public/index.html"));
-});
 
 const port = process.env.PORT || 5000;
 
