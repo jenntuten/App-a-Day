9cbdfefeec21ee5de38bb0b253ac76f1bd553af4
jenn210 <jennifer.c.tuten@gmail.com>
2019-01-29 00:59:16 -0600

    update images

--- client/src/components/Lessons/Lesson1.js
+++ client/src/components/Lessons/Lesson1.js
@@ -10,7 +10,7 @@ class Lesson1 extends Component {
       <div className="column"></div>
       <div className="lesson-header"><h1>Lesson 1: Arrays</h1></div>
          <div className="lesson-container">    
-         <img className="lesson-pics" src="../../assets/images/num6.png" style={{ width: 600 }}/>
+         <img className="lesson-pics" src="../../assets/images/Num6.png" style={{ width: 600 }}/>
       <iframe className="editor-container" src="https://stackblitz.com/edit/js-5orkrc?embed=1&file=index.js" view="Editor" title="code editor"></iframe>
                 </div>
                 </div>
