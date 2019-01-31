55ff3c12b25643f9cfc720dcb92d20c3c0cf5ce5
jenn210 <jennifer.c.tuten@gmail.com>
2019-01-31 14:46:53 -0600

    update css

--- client/src/components/Lessons/Lesson1.js
+++ client/src/components/Lessons/Lesson1.js
@@ -5,17 +5,13 @@ class Lesson1 extends Component {
     render() {
         return (
             <div>
-                <div className="ui container">
-                <div className="ui equal width center aligned padded grid">
-      <div className="column"></div>
-      <div className="lesson-header"><h1>Lesson 1: Arrays</h1></div>
+         <div className="lesson-header"><h1>Lesson 1: Arrays</h1></div>
          <div className="lesson-container">    
          <img className="lesson-pics" src="../../assets/images/Num6.png" style={{ width: 600 }}/>
       <iframe className="editor-container" src="https://stackblitz.com/edit/js-sa15mh?embed=1&file=index.js" view="Editor" title="code editor"></iframe>
                 </div>
                 </div>
-            </div>
-            </div>
+
 
       
     
